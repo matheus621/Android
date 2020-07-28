3503050bc4056c5ce7df2366c1db1e1736a09970
matheus621 <matheus621cardoso@gmail.com>
2020-07-27 22:51:31 -0300

    Criação do banco no sqlite

---
+++ app/src/main/java/com/example/prova/database/AppDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.placeholder.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.prototipoprova.model.Cliente
+
+
+@Database(entities = [Cliente::class], version = 1)
+abstract class AppDatabase() : RoomDatabase() {
+
+    abstract fun Dao(): ClientesDao
+
+    companion object {
+        var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "database.db"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE as AppDatabase
+            } else {
+                INSTANCE as AppDatabase
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/prova/database/ClientesDao.kt
@@ -0,0 +1,25 @@
+package com.example.placeholder.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.prototipoprova.model.Cliente
+
+@Dao
+interface ClientesDao {
+
+    @Query("SELECT * FROM CLIENTES")
+    fun getCliente(): Cliente
+
+    @Query("SELECT * FROM CLIENTES")
+    fun getAllLiveCliente(): LiveData<Cliente>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertSimpleUsers(cliente: Cliente?)
+
+    @Query("SELECT * FROM CLIENTES WHERE ID = :id")
+    fun getSimpleCliente(id: Int) : LiveData<Cliente>
+
+}
